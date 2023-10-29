5176961267928e006ddcce0f17cbc96a46d70e99
tata0303 <hopkinsgift7@gmail.com>
2023-10-29 13:15:28 +0100

    updated search in index.js

--- index.js
+++ index.js
@@ -96,4 +96,4 @@ fahrenheitLink.addEventListener("click", displayFahrenheitTemperature);
 let celsiusLink = document.querySelector("#celsius-link");
 celsiusLink.addEventListener("click", displayCelsiusTemperature);
 
-search("New York");
+search("NIGERIA");
